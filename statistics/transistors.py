7f4ef6cef14ff1e59c65bb67f0ac559326ca461a
verkhovtsevaanna <90704469+verkhovtsevaanna@users.noreply.github.com>
2022-06-27 05:09:16 +0300

    Update transistors.py

--- statistics/transistors.py
+++ statistics/transistors.py
@@ -1,10 +1,10 @@
 import pandas as pd
 from matplotlib import pyplot as plt
 from scipy import stats
+from os import path
 
-data = pd.read_excel("transistors_stats\data.xlsx")
-#print(data.describe())
-result = stats.kstest(data, 'norm', (data.mean(), data.std()), N=146)
+data = pd.read_excel(path.join("data.xlsx"),header=None, usecols=[0])  #меняем путь к файлу, отказываемся от использования заголовка, вместо этого вводим индекс для обращения к столбцу
+result = stats.kstest(data[0], 'norm', (data[0].mean(), data[0].std()), N=146)
 print(result)
 print('Так как К-С статистика велика, а p-значение мало, можно заключить, что данные не нормально распределены')
 plt.hist(data, bins = 30)
